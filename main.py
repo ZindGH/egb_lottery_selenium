5085692cf012702ef9af799243a583243b51ca65
ZindGH <zzzRRzzz@mail.ru>
2023-02-16 21:49:10 +0300

    Added logging wait.

--- main.py
+++ main.py
@@ -24,21 +24,30 @@ else:
     driver = webdriver.Chrome(options=options)
 
 
-
 # Cookie way of saving session
-def open_cookies(driver):
-    cookies = pickle.load(open("cookies.pkl", "rb"))
-    for cookie in cookies:
-        driver.add_cookie(cookie)
-    return driver
+# def open_cookies(driver):
+#     cookies = pickle.load(open("cookies.pkl", "rb"))
+#     for cookie in cookies:
+#         driver.add_cookie(cookie)
+#     return driver
+#
+#
+# def save_cookie(driver):
+#     ref_xpath = '/html/body/div[2]/div[1]/div[1]/div/div[3]/div[2]/div/a/span[1]'
+#     driver.get('http://egbet.live')
+#     wait = ui.WebDriverWait(driver, 30)  # Wait until registration passed
+#     wait.until(expected_conditions.presence_of_element_located((By.XPATH, ref_xpath)))
+#     pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
+#     return driver
 
-
-def save_cookie(driver):
-    ref_xpath = '/html/body/div[2]/div[1]/div[1]/div/div[3]/div[2]/div/a/span[1]'
-    driver.get('http://egbet.live')
-    wait = ui.WebDriverWait(driver, 30)  # Wait until registration passed
-    wait.until(expected_conditions.presence_of_element_located((By.XPATH, ref_xpath)))
-    pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
+def logging(driver):
+    driver.get('https://egbet.live')
+    try:
+        # Registration tab by Xpath
+        driver.find_element(by=By.XPATH, value='/html/body/div[2]/div[1]/div[1]/div/div[3]/div[2]/a[2]')
+        time.sleep(60)  # 60s for logging
+    except expected_conditions.NoSuchElementException:
+        pass
     return driver
 
 
@@ -53,17 +62,17 @@ butn_xpaths = [
 ]
 butn_xpaths *= 2  # create 2 cycles
 
-
 rand_int = random.choices(
     population=[2, 3, 4, 2],
     weights=[25, 25, 25, 25]
 )
 error_count = 0
 success_count = 0
+
+logging(driver)
 while True:
-    logging.info(f'Number of success: {success_count}.')
+
     driver.get('https://egbet.live')
-    # time.sleep(60)  # If login required
     for butn_xpath in butn_xpaths:
         try:
             wait_post = ui.WebDriverWait(driver, 60 * (5 + rand_int[0]), poll_frequency=60)
@@ -84,6 +93,7 @@ while True:
 
     try:
         driver.execute_script("arguments[0].click();", butn)
+        logging.info(f'Number of success: {success_count}.')
     except selenium.common.exceptions.ElementClickInterceptedException as error:
         error_count += 1
         logging.exception(f'ElementClickInterceptedException occured; Errorlog: {error}\nError number: {error_count}')
